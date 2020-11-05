9bdbd1ae033bd6fa7581fe21e672479416474e90
June Anthony Asistio <juneanthony.asistio@microchip.com>
2020-11-05 15:19:33 +0800

    deleted unnecessary files

--- dspic33ck-power-pwm-independent.X/build/MA330048_dsPIC33CK_DPPIM/production/sources/common/p33c_pwm.o.d
+++
@@ -1,3 +0,0 @@
-build/MA330048_dsPIC33CK_DPPIM/production/sources/common/p33c_pwm.o:  \
- sources/common/p33c_pwm.c  \
-sources/common/p33c_pwm.h 
--- dspic33ck-power-pwm-independent.X/build/MA330048_dsPIC33CK_DPPIM/production/sources/pwm.o.d
+++
@@ -1,4 +0,0 @@
-build/MA330048_dsPIC33CK_DPPIM/production/sources/pwm.o:  \
-sources/pwm.c  \
- sources/pwm.h  \
-sources/common/p33c_pwm.h 
