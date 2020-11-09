198f040e93d321eb257aaa754e29d6a2c90b7d14
June Anthony Asistio <juneanthony.asistio@microchip.com>
2020-11-09 13:43:20 +0800

    change feature to independent dual pwm mode. duty cycle of PWMxL independent of PWMxH changes upon pressing onboard switch SW1

--- dspic33ck-power-pwm-independent.X/main.c
+++ dspic33ck-power-pwm-independent.X/main.c
@@ -200,15 +200,13 @@ int main(void)
             while(SW_Read() == SW_PRESSED); 
 
             // Update PWM timing registers
-            if(my_pg->PGxPER.value == 20000)   // IF period is set to 200 kHz)
+            if(my_pg->PGxTRIGA.value  == 5000)   // IF PWMxL duty is set to 25%)
             {
-                my_pg->PGxPER.value = 4000;    // set period to 1 MHz
-                my_pg->PGxDC.value = 1000;     // rescale to 25% duty cycle
+                my_pg->PGxTRIGA.value = 10000;    // set PWMxL duty to 50%
             }
-            else
+            else if(my_pg->PGxTRIGA.value  == 10000)
             {
-                my_pg->PGxPER.value = 20000;   // set period to 200 kHz
-                my_pg->PGxDC.value = 5000;     // rescale to 25% duty cycle
+                my_pg->PGxTRIGA.value = 5000;   // set PWMxL duty to 25%
             }
 
             // Set UPDATE bit of master PWM generator #1
